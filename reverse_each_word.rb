c5de60b07cb4c3f70cdabe5b59b55c814a1ec4bd
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-12 17:11:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ def reverse_each_word(string)
   end
   new_array.join(" ")
 end
-def reverse_each_word(string)
+def reverse_each_word_collect(string)
   array = string.split(" ")
   array.collect do |string|
     string.reverse
