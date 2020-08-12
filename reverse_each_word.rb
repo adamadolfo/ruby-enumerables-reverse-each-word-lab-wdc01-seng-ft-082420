0ef3b2e857cf480b30d1a53f6dab321b33b99782
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-12 17:17:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,9 +9,9 @@ def reverse_each_word(string)
 end
 def reverse_each_word(string)
   array = string.split(" ")
-  test_array = []
+  new_array = []
   array.collect do|string| 
-    test_array << string.reverse
+    new_array << string.reverse
   end
-  test_array.join(" ")
+  new_array.join(" ")
 end 
